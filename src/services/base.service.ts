4351b2e905fcf39cb82154f3349b416b28190c9d
binh.buidang <dangbinh4869@gmail.com>
2022-06-10 01:15:56 +0700

    feat: base service.

---
+++ src/services/base.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from "@nestjs/common";
+import { QueryRunner, getConnection, getRepository } from "typeorm";
+
+@Injectable()
+export class BaseService {
+  protected queryRunner: QueryRunner;
+  // tslint:disable-next-line:no-empty
+  constructor() {}
+  public async startTransaction() {
+    this.queryRunner = getConnection().createQueryRunner();
+    await this.queryRunner.connect();
+    await this.queryRunner.startTransaction();
+  }
+
+  public async commitTransaction() {
+    await this.queryRunner.commitTransaction();
+  }
+
+  public async rollbackTransaction() {
+    await this.queryRunner.rollbackTransaction();
+  }
+
+  public async release() {
+    await this.queryRunner.release();
+  }
+
+  public async saveData(value: any) {
+    return await this.queryRunner.manager.save(value);
+  }
+
+  public async updateData(target: any, criteria: any, value: any) {
+    return await this.queryRunner.manager.update(target, criteria, value);
+  }
+
+  public async deleteData(target: any, criteria: any) {
+    return await this.queryRunner.manager.delete(target, criteria);
+  }
+}
